420a234685eb9eed661fa2037821516b77bf6cf7
Lucas Trindade <lucasktrindade@gmail.com>
2017-03-13 00:24:28 -0300

    Adicionando arquivos para componentização

---
+++ src/app/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <h2>Livraria Flip</h2>
+    <a href="#">Carrinho</a>
+  </nav>
+</template>
+
+<script>
+  export default {
+
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
