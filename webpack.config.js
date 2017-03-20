d6aff4f6baca86ea69b324159205c16095fd6b69
Lucas Trindade <lucasktrindade@gmail.com>
2017-03-20 15:31:19 -0300

    Alterando webpack config e tirando comentário de ativação da linha do service worker

--- webpack.config.js
+++ webpack.config.js
@@ -62,6 +62,7 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
+  module.exports.output.publicPath = './dist/'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
