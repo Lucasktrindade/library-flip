63debea3602807af1c0507cde82501c44f3c9ced
Lucas Trindade <lucasktrindade@gmail.com>
2017-03-20 15:26:26 -0300

    Removendo mode history para funcionar no gh page

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          name: '[name].[ext]'
         }
       }
     ],
